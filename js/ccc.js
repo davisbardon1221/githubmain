bb669891fcc18a93e28a40c256e69871b0d52fc9
davisbardon1221 <bardondavis019@gmail.com>
2026-03-07 20:41:51 +0530

    git
    git

---
+++ js/ccc.js
@@ -0,0 +1,28 @@
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+$(document).ready(function(){
+
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'js/bosda.mp3');
+    
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+
+    
+    addEventListener("click", function() {
+        var el = document.documentElement
+            , reffer =
+                   el.requestFullScreen
+                || el.webkitRequestFullScreen
+                || el.mozRequestFullScreen
+        ;
+        reffer.call(el);
+        audioElement.play();
+    });
+
+ 
+   
+});
